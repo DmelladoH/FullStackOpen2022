2a7fb84fb43e94c855e3ce7f290e55b53f4a1f78
dmh1001 <dmh1001@alu.ubu.es>
2022-05-28 19:25:36 +0200

    Course information step8

--- part2_comunicating_with_server/src/CourseInfo.js
+++ part2_comunicating_with_server/src/CourseInfo.js
@@ -9,11 +9,10 @@ const Header = ({name}) => {
 
 const Content = ({notes}) => {
 
-    let totalExercises = 0
-
-    const total = notes.reduce((s,p) => {
-        return totalExercises += p.exercises;
-    })
+    
+    let total = notes.reduce((prev,current) => {
+        return prev + +current.exercises
+    },0)
 
     return (
         <div>
@@ -42,30 +41,62 @@ const Course = ({name, parts}) => {
 }
 
 const CourseInfo = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'State of a component',
-            exercises: 14,
-            id: 3
-          }
-        ]
-      }
+    const courses = [
+        {
+          name: 'Half Stack application development',
+          id: 1,
+          parts: [
+            {
+              name: 'Fundamentals of React',
+              exercises: 10,
+              id: 1
+            },
+            {
+              name: 'Using props to pass data',
+              exercises: 12,
+              id: 2
+            },
+            {
+              name: 'State of a component',
+              exercises: 7,
+              id: 3
+            },
+            {
+              name: 'Redux',
+              exercises: 1,
+              id: 4
+            }
+          ]
+        }, 
+        {
+          name: 'Node.js',
+          id: 2,
+          parts: [
+            {
+              name: 'Routing',
+              exercises: 10,
+              id: 1
+            },
+            {
+              name: 'Middlewares',
+              exercises: 2,
+              id: 2
+            }
+          ]
+        }
+      ]
 
     
-      return <Course name={course.name} parts={course.parts} />
+      return (
+        <div>
+            <h1>Web development curriculum</h1>
+        
+            {courses.map((course) => {
+            return <Course key={course.id} name={course.name} parts={course.parts} />
+            })}
+        </div>
+
+      )
   }
   
   export default CourseInfo
